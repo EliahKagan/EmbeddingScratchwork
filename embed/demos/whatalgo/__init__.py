6a62157e01c9c45c7d4fd91d4501f915154b5b7a
Eliah Kagan <degeneracypressure@gmail.com>
2023-05-07 16:16:55 -0400

    Continue refining WP algo/ds list
    There's lots more refinement to be done, since there is actually a
    subtle differences between the two Wikipedia articles that the
    current design doesn't account for: the data structures article's
    non-leaf bullet points are still usually specific enough to warrant
    inclusion for whatalgo (which is currently not being done), while
    the algorithms article's non-leaf bullet points should continue to
    be omitted.

--- embed/demos/whatalgo/__init__.py
+++ embed/demos/whatalgo/__init__.py
@@ -4,6 +4,7 @@ import logging
 
 import bs4
 import openai
+import re
 import wikipediaapi
 
 _loggger = logging.getLogger(__name__)
@@ -14,13 +15,18 @@ _ARTICLE_TITLES = ['List of algorithms', 'List of data structures']
 _SKIP_SECTIONS = {'See also', 'External links'}
 """Titles of sections not listing specific algorithms and data structures."""
 
+_NAME_PATTERN = re.compile(
+    r'\S(?:[^:,\s]|,(?!\s)|\s(?![-\N{EN DASH}\N{EM DASH}]\s))*',
+)
+"""Regex for the "name" part of "name: summary" or "name, summary"."""
+
 
 def get_known_names():
     """Retrieve some names of algorithms and data structures from Wikiedia."""
     wiki = wikipediaapi.Wikipedia('en', wikipediaapi.ExtractFormat.HTML)
 
-    return [
-        element.text
+    return sorted({
+        _NAME_PATTERN.search(element.text).group()
         for article_title in _ARTICLE_TITLES
         for section in wiki.article(article_title).sections
         if section.title not in _SKIP_SECTIONS
@@ -28,17 +34,4 @@ def get_known_names():
         in bs4.BeautifulSoup(section.full_text(), features='html.parser')
               .find_all('li', recursive=True)
         if not element.find('ul')  # Filter out non-leaf list items.
-    ]
-
-    # for article_title in _ARTICLE_TITLES:
-    #     for section in wiki.article(article_title).sections:
-    #         if section.title in _SKIP_SECTIONS:
-    #             continue
-    #         yield bs4.BeautifulSoup(section.text)
-
-
-    # return wiki.article('List of algorithms').sections
-
-    #article = wiki.article('List of algorithms')
-    #article = wiki.article('List of data structures')
-    #[section for section in article.sections if section.title not in extra]
+    })
